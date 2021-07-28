f4b89c31a9e21b84d2737faebb532578efd004b5
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2021-07-28 19:27:12 +0200

    Days left added 💻

--- public/app.js
+++ public/app.js
@@ -2,6 +2,7 @@ const mainContainer = document.querySelector(".main-container");
 const timeContainer = document.querySelector(".time-container");
 const dialyEventsContainer = document.querySelector(".dialy-events-container");
 const sportsContainer = document.querySelector(".sports-container");
+const daysContainer = document.querySelector(".days-container");
 
 const dialyEventsNumber = document.querySelector(".events-number");
 
@@ -12,6 +13,7 @@ const API_URLS = {
     all: "https://olypi.com/locations/?call=GetAllLocations",
     byId: `https://olypi.com/locations/?call=GetLocation&id=1`,
   },
+  days: "https://olypi.com/days/?call=Status",
   schedule: {
     sportEvents: `https://olypi.com/schedule/?call=SportEvents`,
     todayEvents: "https://olypi.com/schedule/?call=TodaysEvents",
@@ -53,7 +55,8 @@ window.onload = async () => {
   dialyEventsNumber.innerHTML = `First ${todayEvents.length} daily events`;
   todayEvents.forEach((event) => {
     dialyEventsContainer.innerHTML += `
-    <div class="p-2">
+    <div class="card-group">
+    <div class="card p-2">
     <h3 class="text-center">#${event.id}</h3> 
     <p class="mb-0 text-center">${event.event}</p>
     <p class="text-center"><i class="far fa-clock"></i> ${formatTime(
@@ -63,6 +66,7 @@ window.onload = async () => {
       event.sport_id
     }" target="blank" class="d-flex align-items-center justify-content-center text-center btn btn-primary btn-sm">More Info</a>
     </div>
+    </div>
     `;
   });
   let sports = await showData(API_URLS.sports.all);
@@ -70,7 +74,7 @@ window.onload = async () => {
   sports.length = 12;
   sports.forEach((sport) => {
     sportsContainer.innerHTML += `
-    <div class="sport p-3">
+    <div class="card sport p-3">
     <h3 class="text-center">#${(sport.id -= 1)}</h3>
     <p class="text-center">${sport.name}</p>
     <a href="${
@@ -79,5 +83,12 @@ window.onload = async () => {
     </div>
     `;
   });
-  console.log(sports);
+  let days = await showData(API_URLS.days);
+  daysContainer.innerHTML = `
+  <div class="card p-3 w-100 mt-2">
+  <p class="text-center">Started <span class="started-text">${days.day}</span> days ago</p>
+  <p class="text-center"><span class="started-text">${days.days_remaining}</span> days left</p>
+  </div>
+  `;
+  console.log(days);
 };
